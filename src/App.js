1bbfd73427fd3e5ae8a3607a553d0f6c3947eae7
harithaseddik <akkad.haritha@gmail.com>
2022-08-09 11:53:26 +0300

    remove router plugin

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import "./App.css";
 import React from "react";
 import CameraPage from "./components/CameraPage/CameraPage";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <Router>
+
       <div className="App">
         {/* <Route path="cameraPage" component={CameraPage}/> */}
         <CameraPage />
       </div>
-    </Router>
   );
 }
 
